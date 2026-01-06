2f68db625103698c4278f6ac4fa8a9b2e7b99f67
Karan3064 <kssingh2117@gmail.com>
2026-01-07 01:33:23 +0530

    Restore main App layout

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,8 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Demo from './pages/Demo';
+import Partners from './pages/Partners';
+import About from './pages/About';
+import Settings from './pages/Settings';
+
+
 function App() {
   return (
-    <div style={{ color: "white", padding: 40 }}>
-      NurvoSync is LIVE 🚀
-    </div>
+    <Router>
+      <div className="min-h-screen bg-black">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/partners" element={<Partners />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/settings" element={<Settings />} />
+
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
+
 export default App;
